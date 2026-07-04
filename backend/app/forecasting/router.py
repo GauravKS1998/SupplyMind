df0f4474a6a19a997bf969467e9a324e2edd7e55
Gaurav Kumar <gaurav.kumar.sahoo10@gmail.com>
2026-07-04 17:10:36 +0530

    Forecasting module modified according to the new Category, SubCategory and Product Type module

--- backend/app/forecasting/router.py
+++ backend/app/forecasting/router.py
@@ -4,7 +4,13 @@ from sqlalchemy.orm import Session
 
 from app.database.database import get_db
 
-from .service import forecast_product_demand
+from .service import (
+    forecast_product_demand,
+    forecast_by_supplier,
+    forecast_by_category,
+    forecast_by_subcategory,
+    forecast_by_product_type,
+)
 
 router = APIRouter()
 
@@ -12,3 +18,23 @@ router = APIRouter()
 @router.get("/{product_id}")
 def forecast(product_id: int, db: Session = Depends(get_db)):
     return forecast_product_demand(db, product_id)
+
+
+@router.get("/supplier/{supplier_id}")
+def supplier_forecast(supplier_id: int, db: Session = Depends(get_db)):
+    return forecast_by_supplier(db, supplier_id)
+
+
+@router.get("/category/{category_id}")
+def category_forecast(category_id: int, db: Session = Depends(get_db)):
+    return forecast_by_category(db, category_id)
+
+
+@router.get("/subcategory/{subcategory_id}")
+def subcategory_forecast(subcategory_id: int, db: Session = Depends(get_db)):
+    return forecast_by_subcategory(db, subcategory_id)
+
+
+@router.get("/product_type/{product_type_id}")
+def product_type_forecast(product_type_id: int, db: Session = Depends(get_db)):
+    return forecast_by_product_type(db, product_type_id)
--- backend/app/forecasting/service.py
+++ backend/app/forecasting/service.py
@@ -2,8 +2,19 @@ from sqlalchemy.orm import Session
 
 from app.purchase_orders.model import PurchaseOrder
 
+from app.categories.repository import find_by_id as find_category_by_id
+from app.subcategories.repository import find_by_id as find_subcategory_by_id
+from app.product_types.repository import find_by_id as find_product_type_by_id
+from app.products.repository import find_by_id as find_product_by_id
+from app.suppliers.repository import find_by_id as find_supplier_by_id
+
 
 def forecast_product_demand(db: Session, product_id: int):
+    product = find_product_by_id(db, product_id)
+
+    if not product:
+        return {"message": "Product not found"}
+
     purchase_orders = (
         db.query(PurchaseOrder)
         .filter(
@@ -21,6 +32,132 @@ def forecast_product_demand(db: Session, product_id: int):
 
     return {
         "product_id": product_id,
+        "product_name": purchase_orders[0].product.name,
+        "product_sku": purchase_orders[0].product.sku,
+        "supplier_name": purchase_orders[0].product.supplier.name,
+        "category_name": purchase_orders[0].product.category.name,
+        "subcategory_name": purchase_orders[0].product.subcategory.name,
+        "product_type_name": purchase_orders[0].product.product_type.name,
+        "historical_orders": len(purchase_orders),
+        "predicted_demand": predicted_demand,
+    }
+
+
+def forecast_by_supplier(db: Session, supplier_id: int):
+    supplier = find_supplier_by_id(db, supplier_id)
+
+    if not supplier:
+        return {"message": "Supplier not found"}
+
+    purchase_orders = (
+        db.query(PurchaseOrder)
+        .join(PurchaseOrder.product)
+        .filter(
+            PurchaseOrder.status == "COMPLETED",
+            PurchaseOrder.product.has(supplier_id=supplier_id),
+        )
+        .all()
+    )
+
+    if not purchase_orders:
+        return {"message": "No historical supplier data found"}
+
+    total_quantity = sum(po.quantity for po in purchase_orders)
+
+    predicted_demand = round(total_quantity / len(purchase_orders))
+
+    return {
+        "supplier_name": purchase_orders[0].product.supplier.name,
+        "historical_orders": len(purchase_orders),
+        "predicted_demand": predicted_demand,
+    }
+
+
+def forecast_by_category(db: Session, category_id: int):
+    category = find_category_by_id(db, category_id)
+
+    if not category:
+        return {"message": "Category not found"}
+
+    purchase_orders = (
+        db.query(PurchaseOrder)
+        .join(PurchaseOrder.product)
+        .filter(
+            PurchaseOrder.status == "COMPLETED",
+            PurchaseOrder.product.has(category_id=category_id),
+        )
+        .all()
+    )
+
+    if not purchase_orders:
+        return {"message": "No historical category data found"}
+
+    total_quantity = sum(po.quantity for po in purchase_orders)
+
+    predicted_demand = round(total_quantity / len(purchase_orders))
+
+    return {
+        "category_name": purchase_orders[0].product.category.name,
+        "historical_orders": len(purchase_orders),
+        "predicted_demand": predicted_demand,
+    }
+
+
+def forecast_by_subcategory(db: Session, subcategory_id: int):
+    subcategory = find_subcategory_by_id(db, subcategory_id)
+
+    if not subcategory:
+        return {"message": "Sub-category not found"}
+
+    purchase_orders = (
+        db.query(PurchaseOrder)
+        .join(PurchaseOrder.product)
+        .filter(
+            PurchaseOrder.status == "COMPLETED",
+            PurchaseOrder.product.has(subcategory_id=subcategory_id),
+        )
+        .all()
+    )
+
+    if not purchase_orders:
+        return {"message": "No historical sub-category data found"}
+
+    total_quantity = sum(po.quantity for po in purchase_orders)
+
+    predicted_demand = round(total_quantity / len(purchase_orders))
+
+    return {
+        "sub_category_name": purchase_orders[0].product.subcategory.name,
+        "historical_orders": len(purchase_orders),
+        "predicted_demand": predicted_demand,
+    }
+
+
+def forecast_by_product_type(db: Session, product_type_id: int):
+    product_type = find_product_type_by_id(db, product_type_id)
+
+    if not product_type:
+        return {"message": "Category not found"}
+
+    purchase_orders = (
+        db.query(PurchaseOrder)
+        .join(PurchaseOrder.product)
+        .filter(
+            PurchaseOrder.status == "COMPLETED",
+            PurchaseOrder.product.has(product_type_id=product_type_id),
+        )
+        .all()
+    )
+
+    if not purchase_orders:
+        return {"message": "No historical product type data found"}
+
+    total_quantity = sum(po.quantity for po in purchase_orders)
+
+    predicted_demand = round(total_quantity / len(purchase_orders))
+
+    return {
+        "product_type": purchase_orders[0].product.product_type.name,
         "historical_orders": len(purchase_orders),
         "predicted_demand": predicted_demand,
     }
